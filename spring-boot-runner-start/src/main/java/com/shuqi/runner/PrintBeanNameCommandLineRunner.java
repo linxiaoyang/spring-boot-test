528b95ef94d9d9fa57952f47bedd537d143cec78
shuqi <821286843@qq.com>
2017-12-07 20:15:57 +0800

    runner

---
+++ spring-boot-runner-start/src/main/java/com/shuqi/runner/PrintBeanNameCommandLineRunner.java
@@ -0,0 +1,32 @@
+package com.shuqi.runner;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeansException;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+import java.util.stream.Stream;
+
+
+/**
+ * 打印Bean的名称
+ */
+@Component
+@Slf4j
+public class PrintBeanNameCommandLineRunner implements CommandLineRunner, ApplicationContextAware {
+
+    private ApplicationContext applicationContext;
+
+    @Override
+    public void run(String... args) throws Exception {
+        String[] beanNamesForType = this.applicationContext.getBeanNamesForType(Object.class);
+        Stream.of(beanNamesForType).forEach(t -> log.info("bean name:{}", t));
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+}
