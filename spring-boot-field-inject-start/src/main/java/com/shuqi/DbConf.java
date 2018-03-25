faf26bc8e296a8dd3447a4af1a2bac94d0f55740
shuqi <821286843@qq.com>
2018-03-25 15:51:22 +0800

    update

--- spring-boot-field-inject-start/src/main/java/com/shuqi/DbConf.java
+++ spring-boot-field-inject-start/src/main/java/com/shuqi/DbConf.java
@@ -4,6 +4,10 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 
 @Component
 @ConfigurationProperties(prefix="db")
@@ -13,6 +17,22 @@ public class DbConf {
     private String username;
 
     private String password;
+
+    private List<String> ipList;
+
+    private Map<String,String> map;
+
+    private Child child;
+
+
+    @Data
+    public static class Child{
+
+        private String address;
+
+        private String postcode;
+
+    }
 
 
 }
